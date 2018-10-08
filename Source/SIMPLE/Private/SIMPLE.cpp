1e8b7a2e1fce3d3c7400b1cc9ac467da46c774d0
Jakob Weiss <jakob.weiss@tum.de>
2018-10-08 15:20:11 +0200

    Added basic library structure

---
+++ Source/SIMPLE/Private/SIMPLE.cpp
@@ -0,0 +1,52 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "SIMPLE.h"
+#include "Core.h"
+#include "IPluginManager.h"
+#include "ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FSIMPLEModule"
+
+#include <simple/client.hpp>
+#include <simple_msgs/int.hpp>
+
+DEFINE_LOG_CATEGORY(SIMPLE);
+
+void FSIMPLEModule::StartupModule() {
+  // Get the base directory of this plugin
+  FString BaseDir = IPluginManager::Get().FindPlugin("SIMPLE")->GetBaseDir();
+
+  // Add on the relative location of the third party dll and load it
+  FString LibraryPath;
+#if PLATFORM_WINDOWS
+  LibraryPath =
+      FPaths::Combine(*BaseDir, TEXT("ThirdParty/simple/bin/simple.dll"));
+//#elif PLATFORM_MAC
+  //LibraryPath =
+  //    FPaths::Combine(*BaseDir, TEXT("ThirdParty/simple/Mac/bin/simple.dylib"));
+#else
+  static_assert(false, "Other platforms are currently not supported!");
+#endif
+
+  SIMPLELibraryHandle =
+      !LibraryPath.IsEmpty() ? FPlatformProcess::GetDllHandle(*LibraryPath) : nullptr;
+
+  if (SIMPLELibraryHandle) {
+    UE_LOG(SIMPLE, Log, TEXT("Loaded SIMPLE Version %d.%d.%d"), CV_MAJOR_VERSION, CV_MINOR_VERSION,
+           CV_SUBMINOR_VERSION)
+  } else {
+    UE_LOG(SIMPLE, Error,
+           TEXT("Failed to load SIMPLE library! Check your include/lib paths and make sure "
+                "simple.dll along with its dependencies is deployed!"))
+  }
+}
+
+void FSIMPLEModule::ShutdownModule() {
+  // Free the dll handle
+  FPlatformProcess::FreeDllHandle(SIMPLELibraryHandle);
+  SIMPLELibraryHandle = nullptr;
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FSIMPLEModule, SIMPLE)
